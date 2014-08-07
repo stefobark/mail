45edf5c196db3c8b9d06aeb33e542aee34f1ab40
Steven Barker <steve@sphinxsearch.com>
2014-08-07 07:05:23 -0700

    Update form.php

--- form.php
+++ form.php
@@ -1,16 +1,37 @@
-<?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);  
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
 
-echo <<<END
-<form method="POST" action="folders.php">
-  <h1>e-mail address: </h1>
-  <input type="text" name="username" value=""/>
-  <p>
-  <h1>password: </h1>
-  <input type="text" name="password" value=""/>
-  <input type="submit"/>
+    <title>Signin Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="signin.css" rel="stylesheet">
+<script src="/js/ie-emulation-modes-warning.js"></script>
+ <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="/js/ie10-viewport-bug-workaround.js"></script>
+  
+ 	<title>Collect Emails</title>
+</head>
+<body>
+	<div class="container">
+
+	<form method="POST" class="form-signin" role="form" action="folders.php" required autofocus>
+	<h2 class="form-signin-heading">Get G(e)mails</h2>
+  <input type="text" class="form-control" placeholder="put your email here" name="username"/>
+  <input type="text" class="form-control" placeholder="and your password here" name="password" value=""/>
+
+<button class="btn btn-lg btn-primary btn-block" type="submit">Go</button>
+</div>
 </form>
-<hr>
-END;
-?>
+
+</body>
+</html>
